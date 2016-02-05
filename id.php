e0144ae5b246dfe46550eb5fce8cc7e4f59d63ac
desmond1993 <d.o.ellsworth@spartans.nsu.edu>
2016-02-05 12:00:05 -0500

    Create id.php

---
+++ id.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="../CSS/Id.css">
+</head>
+<body>
+<div class="rcorners1">
+
+
+	<div class="row">
+  <div class="col-sm-12">
+  
+    <div class="row">
+    <div class="col-xs-1 col-sm-1"></div>
+      <div class="col-xs-6 col-sm-6 picPadding">
+
+        <img src="../images/defult.jpg" alt="..." class="img-thumbnail ">
+      </div>
+      <div class="col-xs-5 col-sm-5">
+        
+      </div>
+    </div>
+        <div class="row">
+      <div class="col-xs-6 col-sm-6">
+        
+      </div>
+      <div class="col-xs-6 col-sm-6">
+        <img src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100" alt="" title="" />
+      </div>
+    </div>
+        <div class="row top-buffer">
+      <div class=" text-center">
+       LastName, FirstName
+
+      </div>
+    </div>
+            <div class="row">
+      <div class="text-center ">
+        #######
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
